443a93dab374b7ffb2acc1cdc6cf1b567c4462be
UiraGarro <extadexavier@gmail.com>
2025-04-07 09:07:46 -0300

    .

--- registro_visitantes/urls.py
+++ registro_visitantes/urls.py
@@ -16,19 +16,21 @@ Including another URLconf
 """
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from core.views import VisitorViewSet, AccessLogViewSet
+from core.views import VisitorViewSet, AccessLogViewSet, PolicialViewSet, PresencaDiariaViewSet, PolicialPresencaViewSet, PolicialPresencaCheckInViewSet, PolicialPresencaCheckOutViewSet
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = DefaultRouter()
 router.register(r'visitantes', VisitorViewSet)
 router.register(r'acessos', AccessLogViewSet)
+router.register(r'policiais', PolicialViewSet)
+router.register(r'presenca_diaria', PresencaDiariaViewSet)
+router.register(r'policialpresenca', PolicialPresencaViewSet, basename='policialpresenca')
+router.register(r'policialpresenca-checkin', PolicialPresencaCheckInViewSet, basename='policialpresenca-checkin')
+router.register(r'policialpresenca-checkout', PolicialPresencaCheckOutViewSet, basename='policialpresenca-checkout')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include(router.urls))
 ]
-
-
-
-
